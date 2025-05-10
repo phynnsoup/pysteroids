2ad47d88e142f530e811c639798d109ef7b3c7f8
phynnsoup <phynnigan@gmail.com>
2025-05-11 00:40:30 +1000

    drawing ship

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import pygame
 from constants import *
+from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Initialise delta time and clock for a smooth 60 fps
+    dt = 0
+    clock = pygame.time.Clock()
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     # Game Loop
     while True:
@@ -10,7 +17,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 # Keep game at 60 FPS
+
 
 if __name__ == "__main__":
     main()
