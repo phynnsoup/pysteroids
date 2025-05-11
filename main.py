26231ae5f025bbdfe6442ccfd37ee62f447d193d
phynnsoup <phynnigan@gmail.com>
2025-05-11 11:04:24 +1000

    asteroids finally working

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    # Initialise delta time and clock for a smooth 60 fps
-    dt = 0
     clock = pygame.time.Clock()
+
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+    asteroid_field = AsteroidField()
+
     Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+    dt = 0
     # Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+
         updateable.update(dt)
-        for thing in drawable:
-            thing.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000 # Keep game at 60 FPS
 
 
